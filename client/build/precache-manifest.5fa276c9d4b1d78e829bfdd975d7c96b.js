5833ddf2b28dec54f61c71cdefc3de1a9bf60c76
Ralph Sablaon <rsablaon88@gmail.com>
2019-11-04 20:24:22 -0800

    initial commit

---
+++ client/build/precache-manifest.5fa276c9d4b1d78e829bfdd975d7c96b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97fcf7f360ba28a9167e15a3445ccaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8407e113cbbef4ffc612",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0b2c0e2bc2cd68baf7b8",
+    "url": "/static/js/2.5344c683.chunk.js"
+  },
+  {
+    "revision": "8407e113cbbef4ffc612",
+    "url": "/static/js/main.9566999b.chunk.js"
+  },
+  {
+    "revision": "d1ce6ee7800d6c6a4ff4",
+    "url": "/static/js/runtime-main.82623933.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
